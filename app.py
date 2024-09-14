90977f74ce227038f49506c563ff6925d5505b9d
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-09-14 02:22:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,57 @@
 import streamlit as st
-from transformers import pipeline
+from inference import text2image, load_pipeline
+from io import BytesIO
 
-model = pipeline("sentiment-analysis")
+def app():
+    st.header("Text-to-Image Web App")
+    st.subheader("Powered by Hugging Face")
+    user_input = st.text_area(
+        "Enter your text prompt below and click the button to submit."
+    )
 
-def main():
-    st.title("De texto a imagen con SD y HF model Demo")
+    option = st.selectbox(
+        "Select model (in order of processing time)",
+        (
+            "nota-ai/bk-sdm-small",
+            "CompVis/stable-diffusion-v1-4",
+            "runwayml/stable-diffusion-v1-5",
+            "prompthero/openjourney",
+            "hakurei/waifu-diffusion",
+            "stabilityai/stable-diffusion-2-1",
+            "dreamlike-art/dreamlike-photoreal-2.0",
+        ),
+    )
 
-    # Create an input text box
-    input_text = st.text_input("Enter your text", "")
+    with st.form("my_form"):
+        submit = st.form_submit_button(label="Submit text prompt")
 
-    # Create a button to trigger model inference
-    if st.button("Analyze"):
-        # Perform inference using the loaded model
-        result = model(input_text)
-        st.write("Prediction:", result[0]['label'], "| Score:", result[0]['score'])
+    if submit:
+        with st.spinner(text="Generating image ... It may take up to 1 hour."):
+            im, start, end = text2image(prompt=user_input, repo_id=option)
+
+            buf = BytesIO()
+            im.save(buf, format="PNG")
+            byte_im = buf.getvalue()
+
+            hours, rem = divmod(end - start, 3600)
+            minutes, seconds = divmod(rem, 60)
+
+            st.success(
+                "Processing time: {:0>2}:{:0>2}:{:05.2f}.".format(
+                    int(hours), int(minutes), seconds
+                )
+            )
+
+            st.image(im)
+
+            st.download_button(
+                label="Click here to download",
+                data=byte_im,
+                file_name="generated_image.png",
+                mime="image/png",
+            )
 
 if __name__ == "__main__":
-    main()
-
+    # Preload the pipeline for a default model to speed up first request
+    load_pipeline("stabilityai/stable-diffusion-2-1")
+    app()
